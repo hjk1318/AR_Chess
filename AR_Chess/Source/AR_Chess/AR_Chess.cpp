ad9a838851e8930749e504a4204cb9c78e305716
hjk1318 <hjk1318@g.shingu.ac.kr>
2024-10-23 01:06:15 +0900

    [feat] 프로젝트 생성 및 세팅

---
+++ AR_Chess/Source/AR_Chess/AR_Chess.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "AR_Chess.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AR_Chess, "AR_Chess" );
