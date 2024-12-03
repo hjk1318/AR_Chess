9ae933201159a1454f88361c45029f1b8c679e25
hjk1318 <hjk1318@g.shingu.ac.kr>
2024-12-03 14:08:55 +0900

    유니티로 엔진 변경을 위한 언리얼 프로그램 삭제

--- AR_Chess/Source/AR_Chess/AR_Chess.cpp
+++
@@ -1,6 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "AR_Chess.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, AR_Chess, "AR_Chess" );
